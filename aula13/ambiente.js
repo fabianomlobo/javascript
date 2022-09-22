80bb127a71be64c70b1912988eccf1a12ad5af63
Fabiano Lobo <111807765+fabianomlobo@users.noreply.github.com>
2022-09-21 22:11:58 -0300

    fiz o exercício 15

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)  // estrutura de repetição que faz o teste lógico no final
+    c++
+} while (c <= 9)
+console.log(c)
+
+/*
+var c = 1
+while (c <= 9){
+    console.log(`Passo ${c}`)  // estrutura de repetição que faz o teste lógico no início
+    c++
+}
+console.log(c)
+
+*/
+
+
+
+/* 
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
